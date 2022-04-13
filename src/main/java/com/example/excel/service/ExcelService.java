a2683db62821e72c60aa32fe0b004f0ba231f45b
melkor1004 <hieunv10498@gmail.com>
2022-04-13 16:48:25 +0700

    update

--- src/main/java/com/example/excel/service/ExcelService.java
+++ src/main/java/com/example/excel/service/ExcelService.java
@@ -2,10 +2,14 @@ package com.example.excel.service;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
 
 public interface ExcelService {
+    InputStream tempalte(String filename) throws IOException;
+
     Boolean getExcel(String templateFile, Map<String, Object> params, OutputStream os);
 
     Boolean getExcel(String templateFile, String fileName, Map<String, Object> params, HttpServletResponse response);
